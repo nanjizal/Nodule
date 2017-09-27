866b46768efd7311316950b4e4676cba44f709c3
nanjizal <nanjizal@users.noreply.github.com>
2017-09-27 18:20:31 +0100

    Add files via upload

--- Main.hx
+++ Main.hx
@@ -1,10 +1,13 @@
 package;
 import nodule.*;
+import DevTools.*;
 class Main { static public function main():Void { new Main(); }
     public function new(){
         //var tests = [ 'test0','test1','test2','test3','test4','test5','test8'];
-        var tests = ['test3'];
-        for( test in tests ) testParse( test );
+        //var tests = ['test7'];
+        //for( test in tests ) testParse( test );
+        var doxTest = ['cpp','cs','flash','hl','java','js','lua','macro','neko','php','python'];
+        for( test in doxTest ) testParse( test );
     }
     public function testParse( file: String ){
         trace( '_____________');
@@ -14,10 +17,23 @@ class Main { static public function main():Void { new Main(); }
         trace( 'xml input >');
         trace( str );
         trace( '......');
-        var nodule = ReadXML.parse( str );
-        trace( 'children of ' + nodule.firstChild.name );
-        trace( nodule.firstChild.children(new ArrayPair()) );
-        trace( 'attributes of ' + nodule.firstChild.firstChild.name );
-        trace( nodule.firstChild.firstChild.attributes(new ArrayPair()) );
+        var x0: Xml;
+        var x1: Xml;
+        var x2: Xml;
+        var x3: Nodule;
+        var x4: Xml;
+        var s0: String = '';
+        var s1: String = '';
+        measure( x0 = Xml.parse(str) );
+        measure( x1 = haxe.xml.Parser.parse( str ) );
+        measure( x2 = ReadXML.toXml( str ) );
+        measure( x3 = ReadXML.toNodule( str ) );
+        measure( x4 = ReadXML.toNoduleAndXML( str ) );
+        measure( s0 = x3.toStringPretty() ); 
+        measure( s1 = x0.toString() );
+        trace( 'nodule to string ' + s0 );
+        trace( 'xml to string ' + s1 );
+        //trace( x1.toString() );
+        //trace( x4.toString() );
     }
 }
--- README.md
+++ README.md
@@ -1,23 +1,10 @@
 # Nodule
 low level ( char parsing ) generalised XML parser for Haxe
 
-The trace output of the main example, you can see how it's fairly easy to extract information, probably needs more thought in terms of accessing data, but I think perhaps it could make a good base for custom parsing of dynamic xml.
+The trace output of the main example, you can see how it's fairly easy to extract information.
 
 nodule.Nodule is used to store nodes or attributes with data stored as a double linked tree structure, it keeps access to it's 'sisters' and it's firstChild and firstAttribute and to it's parent.
 
+Added a parser that creates an XML structure.
 
-```
-_____________  _.js:46:3
-testing test3  _.js:47:3
-_____________  _.js:48:3
-xml input >  _.js:50:3
-<?xml version="1.0" encoding="UTF-8"?>
-<family>
-    <father name = "Kawasaki" test = "bill" test2="fred">Yasuhisa</father>
-</family>  _.js:51:3
-......  _.js:52:3
-children of family  _.js:162:3
-( name: father, value: Yasuhisa )  _.js:170:3
-attributes of father  _.js:171:3
-( name: name, value: Kawasaki, name: test, value: bill, name: test2, value: fred )  _.js:179:3
-```
+Added toString for Nodule.
